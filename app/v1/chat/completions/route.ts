b746adb306df36eb6e3a81ad1f179750be85c553
Hai Chang <haichang@microsoft.com>
2023-07-17 14:14:28 +0800

    Add retry policy to the proxy router

--- app/v1/chat/completions/route.ts
+++ app/v1/chat/completions/route.ts
@@ -1,17 +1,39 @@
-import { NextRequest, NextResponse } from 'next/server';
+import {NextRequest, NextResponse} from 'next/server';
+
+const DEFAULT_API_VERSION = '2023-05-15'
+const MAX_RETRY_COUNT = 3;
+const RETRY_DELAY = 1000;
 
 export async function POST(request: NextRequest) {
-  const DEFAULT_API_VERSION = '2023-05-15'
   const apiKey = request.headers.get('authorization')?.replace('Bearer ', '')
   if (!apiKey) {
-    return NextResponse.json({ message: 'Unauthenticated' }, { status: 401 })
+    return NextResponse.json({message: 'Unauthenticated'}, {status: 401})
   }
   const body = await request.json();
+
+  let retryCount = 0;
+  while (true) {
+    let response = await chat(apiKey, body);
+    const status = response.status;
+    if (status < 300 || status === 400) {
+      return response;
+    }
+    if (retryCount >= MAX_RETRY_COUNT) {
+      return response;
+    } else {
+      retryCount++;
+      console.log(`Status is ${status}, Retry ${retryCount} times`);
+      await delay(RETRY_DELAY);
+    }
+  }
+}
+
+export async function chat(apiKey: string, body: any) {
   const [resourceId, mapping, azureApiKey, apiVersion] = apiKey.split(':')
   const model = body['model'];
 
   // get deployment id
-  var deploymentId;
+  let deploymentId;
   if (mapping.includes('|')) {
     const modelMapping = Object.fromEntries(mapping.split(',').map(pair => pair.split('|')));
     deploymentId = modelMapping[model] || Object.values(modelMapping)[0];
@@ -35,12 +57,12 @@ export async function POST(request: NextRequest) {
       async pull(controller) {
         const reader = response.body!.getReader();
         while (true) {
-          const { value, done } = await reader.read();
+          const {value, done} = await reader.read();
           if (done) {
-            // console.log('close controller')
+            console.log('close controller')
             controller.close();
           }
-          // console.log('Received', decoder.decode(value));
+          console.log('Received', decoder.decode(value));
           controller.enqueue(value);
         }
       }
@@ -57,4 +79,8 @@ export async function POST(request: NextRequest) {
     status: response.status,
     headers: response.headers,
   });
+}
+
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
